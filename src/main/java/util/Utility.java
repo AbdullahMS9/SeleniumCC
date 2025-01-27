e4a003c362137710e038869c03f50ec2de6cfb12
AbdullahMS9 <65154836+AbdullahMS9@users.noreply.github.com>
2025-01-27 14:04:20 -0500

    JS Radio Button Test

--- src/main/java/util/Utility.java
+++ src/main/java/util/Utility.java
@@ -1,4 +1,12 @@
 package util;
 
+import com.base.BasePage;
+import org.openqa.selenium.WebDriver;
+
 public class Utility { //1:39:00
+    public static WebDriver driver;
+
+    public static void setUtilityDriver(){
+        driver = BasePage.driver;
+    }
 }
--- src/test/java/part3_4/com/demoqa/base/BaseTest.java
+++ src/test/java/part3_4/com/demoqa/base/BaseTest.java
@@ -1,12 +1,14 @@
 package part3_4.com.demoqa.base;
 
 import com.demoqa.pages.HomePage;
-import com.saucedemo.pages.BasePage;
+import com.base.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+
+import static util.Utility.setUtilityDriver;
 
 public class BaseTest {
     private WebDriver driver;
@@ -25,6 +27,7 @@ public class BaseTest {
         driver.get(DEMOQA_URL);
         basePage = new BasePage();
         basePage.setDriver(driver);
+        setUtilityDriver();
         homePage = new HomePage();
     }
 
