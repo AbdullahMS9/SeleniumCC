b0dcabd19d274a8dba7f46db639ab61924672a02
AbdullahMS9 <65154836+AbdullahMS9@users.noreply.github.com>
2025-01-27 17:15:58 -0500

    Screenshot when failed test

--- src/main/java/com/demoqa/pages/forms/PracticeFormPage.java
+++ src/main/java/com/demoqa/pages/forms/PracticeFormPage.java
@@ -7,6 +7,7 @@ import static util.JavaScriptUtilities.*;
 public class PracticeFormPage extends FormsPage{
 
     private By femaleRadioButton = By.id("gender-radio-2");
+    private By submitButton = By.id("submit");
 
     public void clickFemaleRadioButton() {
         scrollToElementJS(femaleRadioButton);
@@ -15,5 +16,9 @@ public class PracticeFormPage extends FormsPage{
 
     public boolean isFemaleSelected() {
         return find(femaleRadioButton).isSelected();
+    }
+
+    public void clickSubmitButton(){
+        click(submitButton);
     }
 }
--- src/test/java/part3_4/com/demoqa/base/BaseTest.java
+++ src/test/java/part3_4/com/demoqa/base/BaseTest.java
@@ -2,11 +2,19 @@ package part3_4.com.demoqa.base;
 
 import com.demoqa.pages.HomePage;
 import com.base.BasePage;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
 
 import static util.Utility.setUtilityDriver;
 
@@ -29,6 +37,25 @@ public class BaseTest {
         basePage.setDriver(driver);
         setUtilityDriver();
         homePage = new HomePage();
+    }
+
+    @AfterMethod
+    public void takeFailedResultSS(ITestResult testResult){
+        if (testResult.getStatus() == ITestResult.FAILURE){
+            TakesScreenshot screenshot = (TakesScreenshot) driver;
+            File source = screenshot.getScreenshotAs(OutputType.FILE);
+            File destination = new File(System.getProperty("user.dir")
+                    + "/resources/screenshots/(" + java.time.LocalDate.now() +")"
+                    + testResult.getName() +".png");
+            try{
+                FileHandler.copy(source,destination);
+            } catch (IOException e){
+                throw new RuntimeException(e);
+            }
+            System.out.println("Screenshot location: " + destination);
+
+        }
+
     }
 
     @AfterClass
