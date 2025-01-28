59f54c1daa82342ac24543e3246172c9b03938e7
AbdullahMS9 <65154836+AbdullahMS9@users.noreply.github.com>
2025-01-27 21:50:48 -0500

    confirm alert test implemented

--- src/main/java/com/demoqa/pages/alerts_frames_windows/AlertsPage.java
+++ src/main/java/com/demoqa/pages/alerts_frames_windows/AlertsPage.java
@@ -2,11 +2,24 @@ package com.demoqa.pages.alerts_frames_windows;
 
 import org.openqa.selenium.By;
 
+import static util.JavaScriptUtilities.scrollToElementJS;
+
 public class AlertsPage extends Alert_Frames_WindowsPage{
 
     private By informationAlertButton = By.id("alertButton");
+    private By confirmationAlertButton = By.id("confirmButton");
+    private By confirmationResult = By.id("confirmResult");
 
     public void clickInformationAlertButton(){
         click(informationAlertButton);
+    }
+
+    public void clickConfirmationAlertButton() {
+        scrollToElementJS(confirmationAlertButton);
+        click(confirmationAlertButton);
+    }
+
+    public String getConfirmationResult() {
+        return find(confirmationResult).getText();
     }
 }
--- src/test/java/part3_4/com/demoqa/tests/part4/alerts/AlertsTest.java
+++ src/test/java/part3_4/com/demoqa/tests/part4/alerts/AlertsTest.java
@@ -6,14 +6,26 @@ import part3_4.com.demoqa.base.BaseTest;
 
 import static util.SwitchToUtility.*;
 
+@Test
 public class AlertsTest extends BaseTest {
 
-    @Test
     public void testInformationAlert(){
         String expectedAlertText = "You clicked a button";
         var alertsPage = homePage.goToAlertsFramesWindowsCard().clickAlert();
         alertsPage.clickInformationAlertButton();
         Assert.assertEquals(getAlertText(), expectedAlertText,
                 "\nActual and expected messages do not match\n");
+        acceptAlert();
     }
+
+    public void testConfirmationAlert() {
+        var alertsPage = homePage.goToAlertsFramesWindowsCard().clickAlert();
+        alertsPage.clickConfirmationAlertButton();
+        dismissAlert();
+        String actualConfirmationResult = alertsPage.getConfirmationResult();
+        String expectedConfirmationResult = "You selected Cancel";
+        Assert.assertEquals(actualConfirmationResult, expectedConfirmationResult,
+                "\n You Did Not Select Cancel \n");
+    }
+
 }
